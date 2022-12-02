cf1c43c01303f2c15f166ae4a68de5aebc3bc32c
Nile <dncoble@gmail.com>
2022-12-02 10:20:45 -0500

    change main

--- aks.go
+++ aks.go
@@ -1,7 +1,10 @@
-package main
+package aks
 
 import (
+	"fmt"
 	"math"
+	"strconv"
+	"time"
 )
 
 func ModN(N uint, i int) int {
@@ -327,4 +330,28 @@ func AKS(n int) bool {
 	}
 	// step 5 -- for loop
 	return StepFive(n, r)
+}
+func main() {
+	agreeAllNumbers := true
+	start := time.Now()
+	// test first 10,000 numbers
+	for i := 2; i <= 10000; i++ {
+		AKSResult := AKS(i)
+		MillerRabinResult := MillerRabinTest(i)
+		if AKSResult == MillerRabinResult {
+			//if AKSResult {
+			//	fmt.Println("AKS and Miller Rabin agree " + strconv.Itoa(i) + " is prime.")
+			//} else {
+			//	fmt.Println("AKS and Miller Rabin agree " + strconv.Itoa(i) + " is not prime.")
+			//}
+		} else {
+			fmt.Println("AKS and Miller Rabin disagree for " + strconv.Itoa(i))
+			agreeAllNumbers = false
+		}
+	}
+	if agreeAllNumbers {
+		fmt.Println("AKS and Miller Rabin tests agree for integers up to 10,000")
+	}
+	timeElapsed := time.Since(start)
+	fmt.Printf("Time elapsed: %s\n", timeElapsed)
 }
--- main.go
+++ main.go
@@ -1,10 +1,7 @@
-package main
+package aks
 
 import (
-	"fmt"
 	"math"
-	"strconv"
-	"time"
 )
 
 type EuclidData struct {
@@ -111,29 +108,4 @@ func MillerRabinWitness(N, a int) bool {
 	// 8. End i loop.
 	// 9. Return Composite
 	return true
-}
-
-func main() {
-	agreeAllNumbers := true
-	start := time.Now()
-	// test first 10,000 numbers
-	for i := 2; i <= 10000; i++ {
-		AKSResult := AKS(i)
-		MillerRabinResult := MillerRabinTest(i)
-		if AKSResult == MillerRabinResult {
-			//if AKSResult {
-			//	fmt.Println("AKS and Miller Rabin agree " + strconv.Itoa(i) + " is prime.")
-			//} else {
-			//	fmt.Println("AKS and Miller Rabin agree " + strconv.Itoa(i) + " is not prime.")
-			//}
-		} else {
-			fmt.Println("AKS and Miller Rabin disagree for " + strconv.Itoa(i))
-			agreeAllNumbers = false
-		}
-	}
-	if agreeAllNumbers {
-		fmt.Println("AKS and Miller Rabin tests agree for integers up to 10,000")
-	}
-	timeElapsed := time.Since(start)
-	fmt.Printf("Time elapsed: %s\n", timeElapsed)
 }
